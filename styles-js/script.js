0f845da33f0be968df85236b23d2e86593da484f
younes-47 <101892216+younes-47@users.noreply.github.com>
2023-08-29 19:12:21 +0100

    media querry for header

---
+++ styles-js/script.js
@@ -0,0 +1,24 @@
+// MENU TOGGLE FUNCTION
+const navLinks = document.querySelector('.nav-links');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.open-menu');
+const menu_items = document.querySelectorAll('nav .nav-links li a');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    navLinks.style.display = 'flex';
+    navLinks.style.top = '0';
+}
+function close(){
+    navLinks.style.top = '-100%';
+}
