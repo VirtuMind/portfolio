8996b7bbd851025e7faf6680ffe66db9eff83002
Younes <101892216+younes-47@users.noreply.github.com>
2024-08-01 23:29:17 +0100

    whole bunch of styling

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import App from './App';
 import './style.css';
 import './responsive.css';
 
+import 'animate.css';
+
+import 'react-slideshow-image/dist/styles.css';
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/captions.css';
+
 // import i18n (needs to be bundled)
 import '../utils/i18n';
 
