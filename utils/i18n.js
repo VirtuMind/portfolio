2797d0aa04d99a6faa4b5264534cbb5d0c2ff79e
Younes <101892216+younes-47@users.noreply.github.com>
2024-08-01 23:27:57 +0100

    added necessary translations

--- utils/i18n.js
+++ utils/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './../locales/en.json';
 import fr from './../locales/fr.json';
+import de from './../locales/de.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -12,6 +13,9 @@ const resources = {
   },
   fr: {
     translation: fr,
+  },
+  de: {
+    translation: de,
   },
 };
 
