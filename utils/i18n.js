9e00de1e6264b81fd6076497f703e429a5bab415
Younes <101892216+younes-47@users.noreply.github.com>
2024-08-09 01:40:43 +0100

    Detection of the lang from the browser

--- utils/i18n.js
+++ utils/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './../locales/en.json';
 import fr from './../locales/fr.json';
 import de from './../locales/de.json';
@@ -20,13 +21,47 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
     fallbackLng: 'en',
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+    supportedLngs: ['de', 'en', 'fr'],
+
+    // order and from where user language should be detected
+    detection: {
+      order: [
+        'querystring',
+        'cookie',
+        'localStorage',
+        'sessionStorage',
+        'navigator',
+        'htmlTag',
+        'path',
+        'subdomain',
+      ],
+      // keys or params to lookup language from
+      lookupQuerystring: 'lng',
+      lookupCookie: 'i18next',
+      lookupLocalStorage: 'i18nextLng',
+      lookupSessionStorage: 'i18nextLng',
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+
+      // cache user language on
+      caches: ['localStorage', 'cookie'],
+      excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+      // optional expiry and domain for set cookie
+      cookieMinutes: 10,
+      cookieDomain: 'myDomain',
+
+      // optional htmlTag with lang attribute, the default is:
+      htmlTag: document.documentElement,
+
+      // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+      cookieOptions: { path: '/', sameSite: 'strict' },
+    },
 
     interpolation: {
       escapeValue: false, // react already safes from xss
