150437155f8f668f19d4524f49ac9e17d40bd2db
Younes <101892216+VirtuMind@users.noreply.github.com>
2025-05-21 15:09:20 +0100

    Adds Node types and configures path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@hooks': path.resolve(__dirname, 'utils/hooks'),
+      '@utils': path.resolve(__dirname, 'utils'),
+    },
   },
 });
