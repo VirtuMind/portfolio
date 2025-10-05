e30de48137e802646acb7e6b03b82a79fede9fd9
Younes <101892216+VirtuMind@users.noreply.github.com>
2025-10-05 18:48:35 +0100

    Fixed 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,11 +8,11 @@ const NotFound = () => {
   const { language } = useLanguage();
   const t = getTranslations(language);
   return (
-    <>
-      <h1 className="font-mono text-3xl">{t.notFound.title}</h1>
+    <div className="container max-w-3xl mx-auto">
+      <h1 className="font-mono md:text-4xl text-2xl">{t.notFound.title}</h1>
       <p className="mt-4 text-lg mb-6">{t.notFound.message}</p>
       <BackLink />
-    </>
+    </div>
   );
 };
 
